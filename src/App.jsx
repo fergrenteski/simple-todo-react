ad611937ba8c3b15353cbbe199c6ccc404afa28b
Luiz Grenteski <luiz.grenteski@rrs-software.com.br>
2025-02-07 08:29:07 -0300

    :sparkles: feat:  - Dados iniciais.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import { useState } from 'react'
 
 function App() {
+  const [ todos, setTodos ] = useState([
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ]);
+
   return (
-    <div>
-      Olá React!
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map(todo => (
+          <div key={todo.id} className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="categoria">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
